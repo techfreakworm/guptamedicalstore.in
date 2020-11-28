bb055bb932ecffb83ee709e50dd98aef7ad9b165
Mayank Gupta <techfreakworm@gmai.com>
2020-11-28 19:31:31 +0530

    Change testimonials, change images and header of website, make countdown working

--- frontend/js/countdown.js
+++ frontend/js/countdown.js
@@ -1,24 +1,26 @@
-(function($) {
-	$.fn.countdown = function(options, callback) {
+(function ($) {
+	$.fn.countdown = function (options, callback) {
 		//custom 'this' selector
 		thisEl = $(this);
 		//array of custom settings
-		var settings = { 
+		var settings = {
 			'date': null,
 			'format': null
 		};
+		console.log(settings)
 		//append the settings array to options
-		if(options) {
+		if (options) {
 			$.extend(settings, options);
 		}
 		//main countdown function
 		function countdown_proc() {
 			eventDate = Date.parse(settings['date']) / 1000;
-			currentDate = Math.floor($.now() / 1000);
-			if(eventDate <= currentDate) {
+			currentDate = Math.floor(Date.now() / 1000);
+			if (eventDate <= currentDate) {
 				callback.call(this);
 				clearInterval(interval);
 			}
+			// debugger
 			seconds = eventDate - currentDate;
 			days = Math.floor(seconds / (60 * 60 * 24)); //calculate the number of days
 			seconds -= days * 60 * 60 * 24; //update the seconds variable with no. of days removed
@@ -32,26 +34,26 @@
 			if (minutes == 1) { thisEl.find(".timeRefMinutes").text("minute"); } else { thisEl.find(".timeRefMinutes").text("minutes"); }
 			if (seconds == 1) { thisEl.find(".timeRefSeconds").text("second"); } else { thisEl.find(".timeRefSeconds").text("seconds"); }
 			//logic for the two_digits ON setting
-			if(settings['format'] == "on") {
+			if (settings['format'] == "on") {
 				days = (String(days).length >= 2) ? days : "0" + days;
 				hours = (String(hours).length >= 2) ? hours : "0" + hours;
 				minutes = (String(minutes).length >= 2) ? minutes : "0" + minutes;
 				seconds = (String(seconds).length >= 2) ? seconds : "0" + seconds;
 			}
 			//update the countdown's html values.
-			if(!isNaN(eventDate)) {
+			if (!isNaN(eventDate)) {
 				thisEl.find(".days").text(days);
 				thisEl.find(".hours").text(hours);
 				thisEl.find(".minutes").text(minutes);
 				thisEl.find(".seconds").text(seconds);
-			} else { 
+			} else {
 				alert("Invalid date. Here's an example: 12 Tuesday 2012 17:30:00");
-				clearInterval(interval); 
+				clearInterval(interval);
 			}
 		}
-		//run the function
-		countdown_proc();
 		//loop the function
 		interval = setInterval(countdown_proc, 1000);
+		//run the function
+		countdown_proc();
 	}
-}) (jQuery);
+})(jQuery);
