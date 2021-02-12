e3405544fa8b7ac271536373eaae46fd9cbaf6c3
Mayank Gupta <techfreakworm@gmai.com>
2021-02-13 03:04:38 +0530

    Update project timeline

--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -37,7 +37,7 @@ $(document).ready(function () {
 $(document).ready(function () {
 	"use strict";
 	$("#countdown").countdown({
-		date: "15 february 2021 12:00:00", /** Enter new date here **/
+		date: "27 April 2021 12:00:00", /** Enter new date here **/
 		format: "on"
 	},
 		function () {
